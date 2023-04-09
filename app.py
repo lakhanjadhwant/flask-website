f7b0cfbb3d2a494e5852d013dae6a84330cebd33
Tanmay Somani <tanmaysomani2003@gmail.com>
2023-04-09 11:16:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,66 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():
-  return render_template('home.html')
+import pickle
+import streamlit as st
+import numpy as np
 
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
-  
-app.run(host='0.0.0.0',debug=True)
+st.header('Book Suggestion System')
+model = pickle.load(open('artifacts/model.pkl', 'rb'))
+book_names = pickle.load(open('artifacts/book_names.pkl', 'rb'))
+final_rating = pickle.load(open('artifacts/final_rating.pkl', 'rb'))
+book_pivot = pickle.load(open('artifacts/book_pivot.pkl', 'rb'))
+
+
+def fetch_poster(suggestion):
+    book_name = []
+    ids_index = []
+    poster_url = []
+
+    for book_id in suggestion:
+        book_name.append(book_pivot.index[book_id])
+
+    for name in book_name[0]:
+        ids = np.where(final_rating['title'] == name)[0][0]
+        ids_index.append(ids)
+
+    for idx in ids_index:
+        url = final_rating.iloc[idx].get('img_url')
+        poster_url.append(url)
+
+    return poster_url
+
+
+def recommend_book(book_name):
+    books_list = []
+    book_id = np.where(book_pivot.index == book_name)[0][0]
+    distance, suggestion = model.kneighbors(
+        book_pivot.iloc[book_id, :].values.reshape(1, -1), n_neighbors=6)
+
+    poster_url = fetch_poster(suggestion)
+
+    for i in range(len(suggestion)):
+        books = book_pivot.index[suggestion[i]]
+        for j in books:
+            books_list.append(j)
+    return books_list, poster_url
+
+
+selected_books = st.selectbox(
+    "Choose your Chances",
+    book_names
+)
+
+if st.button('Show Recommendations'):
+    recommended_books, poster_url = recommend_book(selected_books)
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.text(recommended_books[1])
+        st.image(poster_url[1])
+    with col2:
+        st.text(recommended_books[2])
+        st.image(poster_url[2])
+    with col3:
+        st.text(recommended_books[3])
+        st.image(poster_url[3])
+    with col4:
+        st.text(recommended_books[4])
+        st.image(poster_url[4])
